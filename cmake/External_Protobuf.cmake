3c9b0c6e267580cfe2ed1bdacf684aa302f0df5c
carlos osuna <carlos.osuna@meteoswiss.ch>
2018-01-10 22:48:54 +0100

    relax python version required

--- cmake/External_Protobuf.cmake
+++ cmake/External_Protobuf.cmake
@@ -53,7 +53,7 @@ function(mchbuild_external_package)
     SOURCE_DIR source_dir)
 
   # Python protobuf
-  find_package(PythonInterp 3.5 REQUIRED)
+  find_package(PythonInterp 3.4 REQUIRED)
   find_package(bash REQUIRED)
 
   set(PROTOBUF_PROTOC "${install_dir}/bin/protoc")
